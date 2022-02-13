d2154db3b7f37c42b402c84465f420cee3809081
GeoEngel <GeoEngel.z@gmail.com>
2022-02-13 14:49:06 -0500

    Infect script now includes newly purchased servers!

--- Scripts/infect.js
+++ Scripts/infect.js
@@ -1,22 +1,13 @@
 import {
-	WEAKEN, GROW, HACK
+	WEAKEN, GROW, HACK, SERVER_LIST
 } from "lib/customConstants.js";
-
-const serverList = [
-	"n00dles","foodnstuff","sigma-cosmetics","joesguns","hong-fang-tea","harakiri-sushi","iron-gym",
-	"darkweb","zer0","nectar-net","max-hardware","CSEC","neo-net","silver-helix","phantasy","omega-net",
-	"the-hub","johnson-ortho","avmnite-02h","comptek","crush-fitness","netlink","syscore","summit-uni",
-	"rothman-uni","catalyst","zb-institute","I.I.I.I","rho-construction","alpha-ent","millenium-fitness",
-	"aevum-police","lexo-corp","aerocorp","galactic-cyber","global-pharm","snap-fitness","deltaone",
-	"unitalife","omnia","icarus","univ-energy","solaris","defcomm","zeus-med","infocomm","taiyang-digital",
-	"zb-def","nova-med","microdyne","applied-energetics","run4theh111z","titan-labs","stormtech","helios",
-	"fulcrumtech","vitalife","omnitek","4sigma","kuai-gong",".","blade","clarkinc","nwo","b-and-a",
-	"powerhouse-fitness","ecorp","fulcrumassets","The-Cave","megacorp"]
+import { getPuchasedServers } from "lib/util.js"
 
 export async function main(ns) {
+	var totalServerList = SERVER_LIST.concat(getPuchasedServers(ns));
 	// Go through all servers & infect them!
-	for(let i = 0; i<serverList.length; i++){
-		await infectVulnerableServer(ns, serverList[i]);
+	for (let i = 0; i < totalServerList.length; i++) {
+		await infectVulnerableServer(ns, totalServerList[i]);
 	}
 }
 
