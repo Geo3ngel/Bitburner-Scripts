d3bb401fa05761880f0bb13af0313438fb82bcb6
GeoEngel <GeoEngel.z@gmail.com>
2022-02-13 02:13:23 -0500

    Temp correction for finding the ccts.

--- Scripts/cctSolver.js
+++ Scripts/cctSolver.js
@@ -13,32 +13,33 @@ export async function main(ns) {
 		for (let i in serversWithCct) {
             var contract = serversWithCct[i];
             var contractType = ns.codingcontract.getContractType(contract[0], contract[1]);
-            switch (contractType) {
-                case "Find Largest Prime Factor":
-                    await ns.exec("contract-prime-factor.ns", HOME, 1, contract[0], contract[1]);
-                    break;
-                case "Total Ways to Sum":
-                    await ns.exec("contract-total-sum.ns", HOME, 1, contract[0], contract[1]);
-                    break;
-                case "Array Jumping Game":
-                    await ns.exec("contract-array-jump.ns", HOME, 1, contract[0], contract[1]);
-                    break;
-                case "Algorithmic Stock Trader II":
-                    await ns.exec("contract-stocks-2.ns", HOME, 1, contract[0], contract[1]);
-                    break;
-                case "Unique Paths in a Grid I":
-                    await ns.exec("contract-unique-paths.ns", HOME, 1, contract[0], contract[1]);
-                    break;
-                //case "Unique Paths in a Grid II":
-                //    await ns.exec("contract-unique-paths-2.ns", home, 1, contract[0], contract[1]);
-                //    break;
-                case "Find All Valid Math Expressions":
-                    await ns.exec("contract-valid-expressions.ns", home, 1, contract[0], contract[1]);
-                    break;
-                default:
-                    ns.print(`No match for ${contractType}`)
-                    break;
-            }
+            ns.print(`Found ${contractType} on ${contract[1]}`)
+        //     switch (contractType) {
+        //         case "Find Largest Prime Factor":
+        //             await ns.exec("contract-prime-factor.ns", HOME, 1, contract[0], contract[1]);
+        //             break;
+        //         case "Total Ways to Sum":
+        //             await ns.exec("contract-total-sum.ns", HOME, 1, contract[0], contract[1]);
+        //             break;
+        //         case "Array Jumping Game":
+        //             await ns.exec("contract-array-jump.ns", HOME, 1, contract[0], contract[1]);
+        //             break;
+        //         case "Algorithmic Stock Trader II":
+        //             await ns.exec("contract-stocks-2.ns", HOME, 1, contract[0], contract[1]);
+        //             break;
+        //         case "Unique Paths in a Grid I":
+        //             await ns.exec("contract-unique-paths.ns", HOME, 1, contract[0], contract[1]);
+        //             break;
+        //         //case "Unique Paths in a Grid II":
+        //         //    await ns.exec("contract-unique-paths-2.ns", home, 1, contract[0], contract[1]);
+        //         //    break;
+        //         case "Find All Valid Math Expressions":
+        //             await ns.exec("contract-valid-expressions.ns", home, 1, contract[0], contract[1]);
+        //             break;
+        //         default:
+        //             ns.print(`No match for ${contractType}`)
+        //             break;
+        //     }
         }
         
         await ns.sleep(60000);
@@ -50,7 +51,7 @@ function findCctFiles(ns){
 		let server = SERVER_LIST[i];
 		var serverContracts = ns.ls(server, ".cct");
 		for (let c = 0; c < serverContracts.length;c++){
-			serversWithCct.push([serverContracts[i], server]);
+			serversWithCct.push([serverContracts[c], server]);
 		}
 	}
 }
